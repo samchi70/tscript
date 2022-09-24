48ea16528d20d74dba82c606ff5ad9348f94f61e
Lazaro Cubas <samchi70@users.noreply.github.com>
2022-09-24 13:27:19 -0400

    initial commit

---
+++ example.ts
@@ -0,0 +1,21 @@
+// let myName: string = "CJ";
+// let isCool: boolean = true;
+// let favNumber: number = 42;
+
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+
+//myName = 42;
+
+// Inferred types
+
+let myName = "CJ";
+let isCool = true;
+let favNumber = 42;
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+console.log(add(4,2));
